8cbdbf474496b46af8db22353c45ab76f3417350
Porfirio Matias <SSJ6Porfy@hotmail.com>
2018-02-26 18:27:38 -0800

    adjusted backend for production

--- server/models/index.js
+++ server/models/index.js
@@ -11,8 +11,7 @@ var db        = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  console.log(config);
-  sequelize = new Sequelize(config.database);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
