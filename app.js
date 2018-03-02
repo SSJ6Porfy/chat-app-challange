422efd52417c51f12e25e457d86d8f025c350388
Porfirio Matias <SSJ6Porfy@hotmail.com>
2018-03-02 00:54:24 -0800

    synced sequelize

--- app.js
+++ app.js
@@ -34,8 +34,18 @@ app.get('/', (req, res) => {
   res.render(path.join(__dirname, '/frontend/static/index.ejs'));
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+db.db.sequelize
+
+.sync({
+      //force: true
+  })
+//.sync()
+.then(function() {
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 });
+
+
 
 module.exports = app;
